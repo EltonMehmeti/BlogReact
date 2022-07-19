c2bc99af0bc531446ddb20d7a8f6417befe4498f
Elton Mehmeti <97539294+EltonMehmeti@users.noreply.github.com>
2022-07-19 13:59:02 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.scss';
+import User from './components/User';
+import Header from './components/Header'
+function App() {
+  return (
+   
+  <>
+<Header />
+  <User />
+  </>
+  
+  );
+}
+
+export default App;
---
+++ src/components/User.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Data } from './data'
+import './user.scss';
+import Item from './Item';
+import State from './State';
+
+
+const User = (props) => {
+  return (
+
+    <div className='container'>
+    <div className='user' >
+{Data.map((d, i,) => {
+return(
+   <Item 
+   key={i}{...d}
+   />
+
+)
+})}
+
+</div>
+
+</div>
+  )
+}
+
+export default User
